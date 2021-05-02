c653e1de7cd0dc230b58c80e5e6b2502fb777f44
Muhamad Zainal Arifin <zainalarifin080718@gmail.com>
2021-05-02 09:50:54 +0700

    Update : Content

--- components/Sections/BlogSection.vue
+++ components/Sections/BlogSection.vue
@@ -2,7 +2,6 @@
   <section class="blog-section pb-8 pt-50">
     <SubContainer sectionTitle="Blog" SectionDescription="  Short story excerpts to fill my spare time about Coding, Work, Your life is everything I want "/>
       <ul class="blogs">
-          <h2 class="text-gray-100 text-3xl lg:text-center font-bold mb-8  mt-10">- Content Not Found -</h2>
         <blog-card v-for="blog in blogs" :key="blog.name" :blog="blog" />
       </ul>
   </section>
