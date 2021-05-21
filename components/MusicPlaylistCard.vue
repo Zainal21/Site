f2805a8100bec2428f890b8ca21af62a3c9a0181
Muhamad Zainal Arifin <zainalarifin080718@gmail.com>
2021-05-21 10:36:59 +0700

    Revert "chore : remove lazy irame"
    This reverts commit f462cf6056021c0cf35970f6e3257da592b830d7.

--- components/MusicPlaylistCard.vue
+++ components/MusicPlaylistCard.vue
@@ -3,7 +3,7 @@
     <iframe width="560" height="315" class="object-cover object-center w-full h-48 mx-auto rounded-lg"
      :src="source" title="YouTube video player" frameborder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-      allowfullscreen></iframe>
+      allowfullscreen v-lazy-load></iframe>
     <div class="mt-2">
       <span class="text-md font-medium px-3 py-3 text-gray-300  dark:text-gray-200">{{title}}</span>
     </div>
