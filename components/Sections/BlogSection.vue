02cfe18d4217762ffc5ebe77e594d09c77af5f8b
Muhamad Zainal Arifin <zainalarifin080718@gmail.com>
2021-06-20 10:14:36 +0700

    chore : change description title content

--- components/Sections/BlogSection.vue
+++ components/Sections/BlogSection.vue
@@ -2,7 +2,7 @@
   <section class="blog-section pb-8 pt-50 ">
     <SubContainer 
         sectionTitle="Blog" 
-        SectionDescription="  Short story excerpts to fill my spare time about Coding, Work, Your life is everything I want "
+        SectionDescription=" Talk about business or sharing about with me? can contact me on various platforms to discuss business with you "
       />
       <ul class="blogs">
         <blog-card 
--- components/Sections/SubContainer.vue
+++ components/Sections/SubContainer.vue
@@ -2,7 +2,7 @@
   <div class="container mx-auto flex justify-between py-4">
     <div class="w-full mb-1 text-center ">
       <div class="text-white text-center lg:flex lg:max-w-5xl">
-        <div class="py-3  text-center">
+        <div class="py-3 text-center">
           <h3 class="text-2xl text-gray-100 font-bold md:text-3xl">{{sectionTitle}}</h3>
           <h4 class="mt-4 text-xl py-3 text-gray-100">{{SectionDescription}}</h4>
         </div>
